700dbf23964db579303f6021a652a03c5d6e1d8b
JanardhanReddy <bommireddy246@gmail.com>
2023-08-01 14:16:41 +0100

    terraform

--- main.tf
+++ main.tf
@@ -9,7 +9,9 @@
 
 module "vpc" {
   source = "git::https://github.com/janardhanReddy-B/tf-module-vpc-b.git"
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-
+  subnets    = each.value["subnets"]
+  env            = var.env
+  tags           = var.tags
 }
