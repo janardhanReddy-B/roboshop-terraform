14eb79593878bd8116938e65c0186ac998d64bf7
JanardhanReddy <bommireddy246@gmail.com>
2023-08-01 01:44:55 +0100

    terraform

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ module "instances" {
   source = "git::https://github.com/janardhanReddy-B/tf-module-app.git"
   component = each.key
   env = var.env
-  tags = each.value["tags"]
+  tags = merge(each.value["tags"])
 
 }
