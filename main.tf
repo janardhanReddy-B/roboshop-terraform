4176d91fb411271d55c7bb995590227136dff290
JanardhanReddy <bommireddy246@gmail.com>
2023-08-02 12:31:49 +0100

    terraform

--- main.tf
+++ main.tf
@@ -7,11 +7,35 @@
 #
 #}
 
+
+
 module "vpc" {
   source = "git::https://github.com/janardhanReddy-B/tf-module-vpc-b.git"
+
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
   subnets    = each.value["subnets"]
+
   env            = var.env
   tags           = var.tags
+  default_vpc_id = var.default_vpc_id
+  default_vpc_rt = var.default_vpc_rt
+
+}
+
+module "rabbitmq" {
+  source = "git::https://github.com/janardhanReddy-B/tf-module-rabbitmq-b.git"
+
+  for_each      = var.rabbitmq
+  component     = each.value["component"]
+  instance_type = each.value["instance_type"]
+
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  subnet_id      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)[0]
+
+  env            = var.env
+  tags           = var.tags
+  allow_ssh_cidr = var.allow_ssh_cidr
+  zone_id        = var.zone_id
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,8 @@
-variable "components" {}
 variable "env" {}
 variable "tags" {}
-
 variable "vpc" {}
+variable "default_vpc_id" {}
+variable "default_vpc_rt" {}
+variable "rabbitmq" {}
+variable "allow_ssh_cidr" {}
+variable "zone_id" {}
